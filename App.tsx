f0e002b326dfc82f7a5846de722d81e6016343df
Nikita Max <iamlegkiy@gmail.com>
2023-03-03 17:54:47 +0800

    asd

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>NIKITA HUI SOSI</Text>
+      <Text>NIKITA HUI SOsaddasSI</Text>
       <StatusBar style="auto" />
     </View>
   );
